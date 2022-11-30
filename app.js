3caf03cc6ba152aa16561815eb52a4ce0692edd2
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-30 17:17:22 +0100

    Add Axios Call

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            tasks: [],
+            upi_url: 'server.php',
         }
+    },
+    methods: {
+        readTasks(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(url);
+                    console.log(response);
+                })
+        }
+    },
+    mounted() {
+        this.readTasks(this.api_url);
     }
 }).mount('#app')
